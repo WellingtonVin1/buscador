35514ca6219808d089fbd169efe4f6a91eae4fcb
Wellington Vinicios <wellingtonv.ti.si@gmail.com>
2023-03-23 17:17:15 -0400

    usando bibliotecas

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
+import { FiSearch } from 'react-icons/fi';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="title">Buscador de CEP</h1>
+
+      <div className="containerInput">
+         <input 
+         type="text"
+         placeholder="Digite seu CEP..."
+         />
+
+         <button className="buttonSearch">
+          <FiSearch size={25} color="#FFF"/>
+         </button>
+
+
+      </div> 
     </div>
   );
 }
