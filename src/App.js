d4ea9126a3f7ad6e192577470ab648f370626c02
Wellington Vinicios <wellingtonv.ti.si@gmail.com>
2023-03-28 23:03:54 -0400

    usando a api de ceps

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useState }from 'react';
 import { FiSearch } from 'react-icons/fi';
 import './styles.css';
 
+import  api  from './services/api';
+
+
 function App() {
+
+  const [input, setInput] = useState('')
+
+//01310930/json/
+
+ async function handleSearch (){
+    
+    if (input === ''){
+      alert("Preencha com algum Cep")
+      return;
+    }
+
+    try{
+        const response = await api.get(`${input}/json`);
+        console.log(response.data);
+    }
+    catch{
+        alert('Ops erro ao Buscar');
+    }
+
+
+  }
+
+
   return (
     <div className="container">
       <h1 className="title">Buscador de CEP</h1>
@@ -10,9 +38,11 @@ function App() {
          <input 
          type="text"
          placeholder="Digite seu CEP..."
+         value={input}
+         onChange={(e) => setInput(e.target.value)}
          />
 
-         <button className="buttonSearch">
+         <button className="buttonSearch" onClick={handleSearch}>
           <FiSearch size={25} color="#FFF"/>
          </button>
       </div> 
